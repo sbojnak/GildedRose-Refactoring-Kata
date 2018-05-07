3df201cf831b251eac0fbe264af4662f92b50a06
Stefan Bojnak <xbojnak@fi.muni.cz>
2018-05-07 09:54:02 +0200

    Fixed and renamed first test.

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -7,12 +7,12 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void UpdateQuality_OnZeroSellInAndZeroQuality_MaintainCorrectName()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
         }
     }
 }
